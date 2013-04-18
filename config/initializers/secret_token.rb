89e4a35912e029c64989ae537d8903a8dc67571f
paulo carmona <pcarmonaz@hotmail.com>
2013-04-18 18:25:47 -0500

    Inicio Aplicacion con Manejo de fotos gema paperclip en rails3.2.12

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paperclip::Application.config.secret_token = '43875e34a01bde346d8a09b6c054b70b70bea6a1a29e0e38deeaeafd53eac4f05e7c54c8bd8694efc68df808c08f6572e33fc795b8bbe3a1c9811e622abdf9f9'
