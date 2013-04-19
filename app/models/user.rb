f884a52e3e5caf70743084ffc2c6ca3888c6b27c
paulo carmona <pcarmonaz@hotmail.com>
2013-04-19 00:22:35 -0500

    paso4 - Adicione a user.rb el atributo de paperclip avatar, hagalo accesible y adicionele las rutas de upload
    attr_accessible :avatar
      has_attached_file :avatar,
                        :styles => { :medium => "300x300>", :thumb => "100x100>" },
                        :default_url => "/images/:style/missing.png",
    		    :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
          		    :url => "/system/:attachment/:id/:style/:filename"
    	modified:   app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :rol
+  attr_accessible :avatar
+  has_attached_file :avatar, 
+                    :styles => { :medium => "300x300>", :thumb => "100x100>" },
+                    :default_url => "/images/:style/missing.png",
+		    :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
+      		    :url => "/system/:attachment/:id/:style/:filename"	
 end
